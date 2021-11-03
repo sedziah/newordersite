072b90f43ab99dd9f1fae649bcac33fe96503275
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2021-11-03 23:11:17 +0000

    new update

--- ordersite/wsgi.py
+++ ordersite/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from orderform.models import *
+from django.core.wsgi import get_wsgi_application
 
-from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
 
 
 import ordersite
