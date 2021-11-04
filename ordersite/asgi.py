31fc55ef1674b4655dde7e6923d4206b82dbf335
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2021-11-04 06:55:33 +0000

    new udpdate

--- ordersite/asgi.py
+++ ordersite/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from .asgi import *
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ordersite.settings')
 
