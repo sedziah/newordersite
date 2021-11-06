515152cabdb76fd4d6c3d449c62229ef0b352742
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2021-11-06 09:47:30 +0000

    celery update

--- ordersite/celery.py
+++ ordersite/celery.py
@@ -9,7 +9,7 @@ app = Celery('ordersite')
 
 app.config_from_object('django.conf:settings')#, namespace='CELERY')
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
